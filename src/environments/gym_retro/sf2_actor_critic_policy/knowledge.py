707fad400d4159ff4c848a87f2cec73fbc30d00f
Gabriel Munoz <gabriel.munoz@intelygenz.com>
2019-07-16 18:23:07 +0200

    add shared model stuff

--- src/environments/gym_retro/sf2_actor_critic_policy/knowledge.py
+++ src/environments/gym_retro/sf2_actor_critic_policy/knowledge.py
@@ -1,9 +1,7 @@
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.distributions.categorical import Categorical
 
 
 class ActorCritic(torch.nn.Module):
@@ -14,7 +12,8 @@ class ActorCritic(torch.nn.Module):
         self.train()
 
     def model_structure(self, num_inputs, num_outputs):
-        self.conv1 = nn.Conv2d(num_inputs, 32, kernel_size=2, stride=2, padding=1)
+        self.conv1 = nn.Conv2d(num_inputs, 32,
+                               kernel_size=2, stride=2, padding=1)
         self.conv2 = nn.Conv2d(32, 32, kernel_size=2, stride=2, padding=1)
         self.conv3 = nn.Conv2d(32, 32, kernel_size=2, stride=2, padding=1)
         self.conv4 = nn.Conv2d(32, 32, kernel_size=2, stride=2, padding=1)
@@ -47,6 +46,7 @@ class Knowledge():
         self.action_space = action_space
         self.model = ActorCritic(self.input_frames, self.action_space)
         self.model.double()
+        self.model.train()
 
         self.gamma = 0.9
         self.tau = 1.0
@@ -59,19 +59,33 @@ class Knowledge():
         )
         self.model = self.model.to('cpu')
 
+    def load_shared_model(self, shared_model):
+        self.shared_model = shared_model
+        self.model.load_state_dict(self.shared_model.state_dict())
+
+    def reload_model(self):
+        self.model.load_state_dict(self.shared_model.state_dict())
+
+    def ensure_shared_grads(self):
+        for param, shared_param in zip(self.model.parameters(),
+                                       self.shared_model.parameters()):
+            if shared_param.grad is not None:
+                return
+            shared_param._grad = param.grad
+
     def get_action(self, state):
         policy, _ = self.model(torch.tensor(state).unsqueeze(0))
         action = F.softmax(policy, -1).multinomial(num_samples=1)
         return action
 
     def train(self, experiences):
-        states  = torch.tensor(experiences[:, 0].tolist()).double()
+        states = torch.tensor(experiences[:, 0].tolist()).double()
         rewards = torch.tensor(experiences[:, 1].tolist()).double()
         actions = torch.tensor(experiences[:, 2].tolist()).long()
         next_states = torch.tensor(experiences[:, 3].tolist()).double()
 
         logits, values = self.model(states)
-        probs     = F.softmax(logits, -1)
+        probs = F.softmax(logits, -1)
         log_probs = F.log_softmax(logits, -1)
         entropies = -(log_probs * probs).sum(1, keepdim=True)
         log_probs = log_probs.gather(1, actions.unsqueeze(1))
@@ -89,11 +103,19 @@ class Knowledge():
             value_loss = value_loss + 0.5 * advantage.pow(2)
 
             # Generalized Advantage Estimation
-            delta_t = rewards[i] + self.gamma * values[i + 1].data - values[i].data
+            delta_t = (
+                rewards[i] +
+                self.gamma * values[i + 1].data -
+                values[i].data)
             gae = gae.double() * self.gamma * self.tau + delta_t
-            policy_loss = policy_loss - (log_probs[i] * gae) - (self.entropy_coef * entropies[i])
+            policy_loss = (
+                policy_loss -
+                (log_probs[i] * gae) -
+                (self.entropy_coef * entropies[i]))
 
         self.optimizer.zero_grad()
         loss_fn = (policy_loss + self.value_loss_coef * value_loss)
         loss_fn.backward()
         self.optimizer.step()
+
+        torch.save(self.shared_model.state_dict(), 'sf2_a3c.pth')
