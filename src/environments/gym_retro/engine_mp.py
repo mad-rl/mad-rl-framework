6adcf5a3f8ea746a23295dcb231686cbefd5414e
Gabriel Munoz <gabriel.munoz@intelygenz.com>
2019-07-16 18:21:25 +0200

    initialize shared model on engine

--- src/environments/gym_retro/engine_mp.py
+++ src/environments/gym_retro/engine_mp.py
@@ -4,31 +4,35 @@ import retro
 import torch.multiprocessing as mp
 
 from core.mad_rl import MAD_RL
+from environments.gym_retro.sf2_actor_critic_policy.knowledge import (
+    ActorCritic)
 
 
 class Engine:
 
     def __init__(self):
-        pass
+        input_frames = 16
+        action_space = 49
+        self.shared_model = ActorCritic(
+            input_frames, num_outputs=action_space)
+        self.shared_model.share_memory()
 
     def train_worker(self, config, game, render):
         env = retro.make(game)
         agent = MAD_RL.agent()
+        agent.knowledge.load_shared_model(self.shared_model)
 
         for episode in range(config["episodes"]):
             game_finished = False
             env.reset()
 
             agent.start_episode(episode)
-
+            observation = env.render(mode='rgb_array')
             step = 0
             while not game_finished:
+                agent.start_step(step)
+                action = agent.get_action(observation)
                 for step in range(config["steps_per_episode"]):
-                    agent.start_step(step)
-
-                    observation = env.render(mode='rgb_array')
-                    action = agent.get_action(observation)
-
                     next_observation, reward, game_finished, info = env.step(
                         action)
 
@@ -36,9 +40,11 @@ class Engine:
                         env.render()
 
                     agent.add_experience(
-                        observation, reward, action, next_observation, info=info)
+                        observation, reward, action, next_observation,
+                        info=info
+                    )
                     agent.end_step(step)
-
+                    observation = next_observation
                     step = step + 1
 
                     if game_finished:
@@ -57,7 +63,8 @@ class Engine:
 
         processes = []
         for rank in range(0, number_of_workers):
-            p = mp.Process(target=self.train_worker, args=(config, game, env_render))
+            p = mp.Process(
+                target=self.train_worker, args=(config, game, env_render))
             p.start()
             processes.append(p)
 
