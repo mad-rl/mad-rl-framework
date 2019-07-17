9e32dd177ab586f5f7687deaf8d16cd33dca292d
Gabriel Munoz <gabriel.munoz@intelygenz.com>
2019-07-17 10:30:22 +0200

    change comparison operator for None values

--- src/core/mad_rl.py
+++ src/core/mad_rl.py
@@ -7,11 +7,13 @@ CONFIG = {
     "episodes": int(os.getenv('EPISODES', 10)),
     "steps_per_episode": int(os.getenv('STEPS_PER_EPISODE', 100)),
     "engine": {
-        "module": os.getenv('ENGINE_MODULE', 'environments._test_env.test_engine'),
+        "module": os.getenv('ENGINE_MODULE',
+                            'environments._test_env.test_engine'),
         "class": os.getenv('ENGINE_CLASS', 'Engine')
     },
     "agent": {
-        "module": os.getenv('AGENT_MODULE', 'environments._test_env._test_agent.agent'),
+        "module": os.getenv('AGENT_MODULE',
+                            'environments._test_env._test_agent.agent'),
         "class_name": os.getenv('AGENT_CLASS', 'Agent')
     }
 }
@@ -28,9 +30,9 @@ class MAD_RL:
     @staticmethod
     def engine(module=None, class_name=None):
         config = MAD_RL.config()
-        if module == None:
+        if module is None:
             module = config["engine"]["module"]
-        if class_name == None:
+        if class_name is None:
             class_name = config["engine"]["class"]
         environment_module = __import__(module, fromlist=["*"])
         environment_class = getattr(environment_module, class_name)
@@ -39,9 +41,9 @@ class MAD_RL:
     @staticmethod
     def agent(module=None, class_name=None, action_space=None):
         config = MAD_RL.config()
-        if module == None:
+        if module is None:
             module = config["agent"]["module"]
-        if class_name == None:
+        if class_name is None:
             class_name = config["agent"]["class_name"]
         agent_mod = __import__(module, fromlist=["*"])
         agent_class = getattr(agent_mod, class_name)
